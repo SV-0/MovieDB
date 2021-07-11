3ed4cb909fb469e76b18d3816bfa1f2d54231789
SV-0 <sakshiverma8100@gmail.com>
2021-07-12 03:54:19 +0530

    add scroll to top function

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
-import React, { Suspense } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
+
 import Auth from "./hoc/auth";
-import LandingPage from "./components/LandingPage/LandingPage.js";
+import LandingPage from "./components/LandingPage/LandingPage";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import RegisterPage from "./components/Auth/RegisterPage";
+import LoginPage from "./components/Auth/LoginPage";
+
 function App() {
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      {/* <NavBar /> */}
-      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
+      <ScrollToTop />
+      <NavBar />
+      <div style={{ minHeight: "calc(100vh - 80px)" }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)} />
         </Switch>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </Suspense>
   );
 }
