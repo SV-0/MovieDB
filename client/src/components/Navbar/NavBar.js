6813dd7349db94a7fcba5c7a55bbd5ebee5974a3
SV-0 <sakshiverma8100@gmail.com>
2021-07-12 04:00:39 +0530

    added nav bar

---
+++ client/src/components/Navbar/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { AppBar, Toolbar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import RightMenu from "./Sections/RightMenu";
+
+import useStyles from "./styles";
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit" elevation={6} width="70%">
+      <Link to="/" className={classes.brandContainer}>
+        MovieDB
+        {/* <img component={Link} to="/" src={text} alt="icon" height="45px" /> */}
+      </Link>
+      <Toolbar className={classes.toolbar} variant="dense">
+        <RightMenu />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
