4b10e74e02a45c15df3ba22f2de294c0565e5106
SV-0 <sakshiverma8100@gmail.com>
2021-07-12 04:08:09 +0530

    added footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div
+      style={{
+        height: "40px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "1rem",
+      }}
+    >
+      <h3> MovieDB</h3>
+    </div>
+  );
+}
+
+export default Footer;
